a452a4065f70fd0947a581915d6ddfec5376c7e2
Michelle Wong <michellekwong2@gmail.com>
2020-05-08 19:14:08 -0400

    Event listeners on add/remove clothing buttons

---
+++ main.js
@@ -0,0 +1,13 @@
+let addButton = document.getElementById('add-clothing');
+addButton.addEventListener("click", addClothing);
+
+function addClothing(){
+    console.log('ADD THE CLOTHING')
+}
+
+let removeButton = document.getElementById('remove-clothing');
+removeButton.addEventListener("click", removeClothing);
+
+function removeClothing(){
+    console.log('REMOVE THE CLOTHING')
+}
