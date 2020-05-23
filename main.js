d5ee6ecb79eaac32a5485ab5d27e74e20150e427
Michelle Wong <michellekwong2@gmail.com>
2020-05-23 16:23:06 -0400

    PATCH fetch request for persisting likes

--- main.js
+++ main.js
@@ -49,20 +49,34 @@ function addItem(item){
     li.className="clothing-item"
     li.innerHTML=`
     <h3>${item.name}</h3>
-        <h4>Score: <span>0</span></h4>
+        <h4>Likes: <span>${item.likes}</span></h4>
         <img alt='' src='${item.image}' />
-        <button class='up-vote' data-purpose="increase">Up Vote</button>
-        <button class='down-vote' data-purpose="decrease">Down Vote</button>
-        <button class='delete-item' data-purpose="delete-item">Remove</button>
+        <button class='up-vote' data-purpose="increase" data-id=${item.id} data-likes=${item.likes}>Up Vote</button>
+        <button class='down-vote' data-purpose="decrease" data-id=${item.id}>Down Vote</button>
+        <button class='delete-item' data-purpose="delete-item"  data-id=${item.id}>Remove</button>
     `
     parentUl.append(li)
 }
 
+//patch needs specific id
+function increaseLikes(id, likes){
+    console.log(id, likes)
+    fetch(`http://localhost:3000/clothing/${id}`, {
+        method: "PATCH",
+        headers: {
+            "content-type" : "application/json",
+            accepts: "application/json"
+        },
+        body: JSON.stringify({likes: likes})
+    })
+    
+}
 
 function changeScore(event){
     let parentLi= event.target.parentNode
     let span = parentLi.querySelector("span")
     let score = event.target.dataset.purpose === "increase" ? parseInt(span.innerText) + 1 : parseInt(span.innerText) -1;
+    event.target.dataset.likes = score
     span.innerText = score
 }
 
@@ -77,5 +91,6 @@ parentUl.addEventListener("click", function(event){
         removeItem(event)
     } else {
         changeScore(event)
+        increaseLikes(event.target.dataset.id, event.target.dataset.likes)
     }
 })
